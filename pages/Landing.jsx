1ea614efbfcbe6de1b2852d8b53eceecd6f6489a
thekitchenhome66-web <thekitchenhome66@gmail.com>
2026-03-17 01:01:35 +0800

    Create Landing.jsx

---
+++ pages/Landing.jsx
@@ -0,0 +1,30 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import Features from "../components/Features"
+import Steps from "../components/Steps"
+import CTA from "../components/CTA"
+import Footer from "../components/Footer"
+
+export default function Landing(){
+
+return (
+
+<div className="bg-white text-gray-900">
+
+<Navbar />
+
+<Hero />
+
+<Features />
+
+<Steps />
+
+<CTA />
+
+<Footer />
+
+</div>
+
+)
+
+}
